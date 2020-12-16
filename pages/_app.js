1e0b55fe431bca36d65ad887c6c584e4731a3e55
Xuezheng Wang <xzzwang@icloud.com>
2020-12-15 21:15:34 -0800

    Under construction sign is up

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+import React from 'react'
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Image } from 'react-bootstrap'
 
 export default function Home() {
   return (
@@ -6,7 +7,12 @@ export default function Home() {
       <Head>
         <title>Xuezheng Wang</title>
       </Head>
-      <h1>Xuezheng Wang</h1>
+      <h2>Xuezheng Wang's Website</h2>
+      <Image src="icons8-under-construction-64.png" />
+      <div>
+        Sorry! This site is still under construction! <br />
+        But here's the link to my resume if you are interested: <a href='XuezhengWangResume.pdf'>Resume</a>
+      </div>
     </>
   )
 }
